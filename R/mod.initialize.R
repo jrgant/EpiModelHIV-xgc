45564a9bf916467727c4d0c58716efb9dfd3e244
smjenness <smjenness@gmail.com>
2018-12-14 15:56:28 -0500

    Move STI inits to separate sub fx

--- R/mod.initialize.R
+++ R/mod.initialize.R
@@ -22,7 +22,7 @@
 #'
 initialize_msm <- function(x, param, init, control, s) {
 
-  # Master data list
+  ## Master Data List Setup ##
   dat <- list()
   dat$param <- param
   dat$init <- init
@@ -38,13 +38,14 @@ initialize_msm <- function(x, param, init, control, s) {
                     EpiModelHIV::InitErgmConstraint..attributes,
                     ns = "ergm", envir = as.environment("package:ergm"))
 
-  ## Network simulation ##
+
+  ## Network Setup ##
   nw <- list()
   for (i in 1:3) {
     nw[[i]] <- simulate(x[[i]]$fit, basis = x[[i]]$fit$newnetwork)
   }
 
-  ## Build initial edgelists
+  # Build initial edgelists
   dat$el <- list()
   dat$p <- list()
   for (i in 1:2) {
@@ -62,7 +63,6 @@ initialize_msm <- function(x, param, init, control, s) {
   p$model.form$formula <- NULL
   dat$p[[3]] <- p
 
-
   # Network parameters
   dat$nwparam <- list()
   for (i in 1:3) {
@@ -70,12 +70,11 @@ initialize_msm <- function(x, param, init, control, s) {
   }
 
 
-  ## Nodal attributes ##
+  ## Nodal Attributes Setup ##
 
   # Degree terms
   dat$attr$deg.pers <- get.vertex.attribute(x[[1]]$fit$network, "deg.pers")
   dat$attr$deg.main <- get.vertex.attribute(x[[2]]$fit$network, "deg.main")
-
 
   # Race
   dat$attr$race <- get.vertex.attribute(nw[[1]], "race")
@@ -135,75 +134,20 @@ initialize_msm <- function(x, param, init, control, s) {
   # HIV-related attributes
   dat <- init_status_msm(dat)
 
-  ## GC/CT status
-  idsUreth <- which(role.class %in% c("I", "V"))
-  idsRect <- which(role.class %in% c("R", "V"))
-
-  uGC <- rGC <- rep(0, num)
-  uCT <- rCT <- rep(0, num)
-
-  # Initialize GC infection at both sites
-  idsUGC <- sample(idsUreth, size = round(init$prev.ugc * num), FALSE)
-  uGC[idsUGC] <- 1
-
-  idsRGC <- sample(setdiff(idsRect, idsUGC), size = round(init$prev.rgc * num), FALSE)
-  rGC[idsRGC] <- 1
-
-  dat$attr$rGC <- rGC
-  dat$attr$uGC <- uGC
-
-  dat$attr$rGC.sympt <- dat$attr$uGC.sympt <- rep(NA, num)
-  dat$attr$rGC.sympt[rGC == 1] <- rbinom(sum(rGC == 1), 1, dat$param$rgc.sympt.prob)
-  dat$attr$uGC.sympt[uGC == 1] <- rbinom(sum(uGC == 1), 1, dat$param$ugc.sympt.prob)
-
-  dat$attr$rGC.infTime <- dat$attr$uGC.infTime <- rep(NA, length(dat$attr$active))
-  dat$attr$rGC.infTime[rGC == 1] <- 1
-  dat$attr$uGC.infTime[uGC == 1] <- 1
-
-  dat$attr$rGC.timesInf <- rep(0, num)
-  dat$attr$rGC.timesInf[rGC == 1] <- 1
-  dat$attr$uGC.timesInf <- rep(0, num)
-  dat$attr$uGC.timesInf[uGC == 1] <- 1
-
-  dat$attr$rGC.tx <- dat$attr$uGC.tx <- rep(NA, num)
-  dat$attr$rGC.tx.prep <- dat$attr$uGC.tx.prep <- rep(NA, num)
-
-  # Initialize CT infection at both sites
-  idsUCT <- sample(idsUreth, size = round(init$prev.uct * num), FALSE)
-  uCT[idsUCT] <- 1
-
-  idsRCT <- sample(setdiff(idsRect, idsUCT), size = round(init$prev.rct * num), FALSE)
-  rCT[idsRCT] <- 1
-
-  dat$attr$rCT <- rCT
-  dat$attr$uCT <- uCT
-
-  dat$attr$rCT.sympt <- dat$attr$uCT.sympt <- rep(NA, num)
-  dat$attr$rCT.sympt[rCT == 1] <- rbinom(sum(rCT == 1), 1, dat$param$rct.sympt.prob)
-  dat$attr$uCT.sympt[uCT == 1] <- rbinom(sum(uCT == 1), 1, dat$param$uct.sympt.prob)
-
-  dat$attr$rCT.infTime <- dat$attr$uCT.infTime <- rep(NA, num)
-  dat$attr$rCT.infTime[dat$attr$rCT == 1] <- 1
-  dat$attr$uCT.infTime[dat$attr$uCT == 1] <- 1
-
-  dat$attr$rCT.timesInf <- rep(0, num)
-  dat$attr$rCT.timesInf[rCT == 1] <- 1
-  dat$attr$uCT.timesInf <- rep(0, num)
-  dat$attr$uCT.timesInf[uCT == 1] <- 1
-
-  dat$attr$rCT.tx <- dat$attr$uCT.tx <- rep(NA, num)
-  dat$attr$rCT.tx.prep <- dat$attr$uCT.tx.prep <- rep(NA, num)
-
+  # STI Status
+  dat <- init_sti_msm(dat)
 
   # Network statistics
   dat$stats$nwstats <- list()
 
 
+  ## Other Setup ##
+
   # Prevalence Tracking
   dat$temp$deg.dists <- list()
   dat <- prevalence_msm(dat, at = 1)
 
-  # Save partner list
+  # Setup Partner List
   plist <- cbind(dat$el[[1]], ptype = 1)
   plist <- rbind(plist, cbind(dat$el[[2]], ptype = 2))
   plist <- cbind(plist, start = 1, stop = NA)
@@ -601,6 +545,84 @@ init_status_msm <- function(dat) {
   dat$attr$inf.diag <- inf.diag
   dat$attr$inf.tx <- inf.tx
   dat$attr$inf.stage <- inf.stage
+
+  return(dat)
+
+}
+
+#' @title Initialize the STI status of persons in the network
+#'
+#' @description Sets the initial individual-level disease status of persons
+#'              in the network, as well as disease-related attributes for
+#'              infected persons.
+#'
+#' @param dat Data object created in initialization module.
+#'
+#' @export
+#' @keywords initiation utility msm
+#'
+init_sti_msm <- function(dat) {
+
+  role.class <- dat$attr$role.class
+  num <- length(role.class)
+
+  idsUreth <- which(role.class %in% c("I", "V"))
+  idsRect <- which(role.class %in% c("R", "V"))
+
+  uGC <- rGC <- rep(0, num)
+  uCT <- rCT <- rep(0, num)
+
+  # Initialize GC infection at both sites
+  idsUGC <- sample(idsUreth, size = round(init$prev.ugc * num), FALSE)
+  uGC[idsUGC] <- 1
+
+  idsRGC <- sample(setdiff(idsRect, idsUGC), size = round(init$prev.rgc * num), FALSE)
+  rGC[idsRGC] <- 1
+
+  dat$attr$rGC <- rGC
+  dat$attr$uGC <- uGC
+
+  dat$attr$rGC.sympt <- dat$attr$uGC.sympt <- rep(NA, num)
+  dat$attr$rGC.sympt[rGC == 1] <- rbinom(sum(rGC == 1), 1, dat$param$rgc.sympt.prob)
+  dat$attr$uGC.sympt[uGC == 1] <- rbinom(sum(uGC == 1), 1, dat$param$ugc.sympt.prob)
+
+  dat$attr$rGC.infTime <- dat$attr$uGC.infTime <- rep(NA, length(dat$attr$active))
+  dat$attr$rGC.infTime[rGC == 1] <- 1
+  dat$attr$uGC.infTime[uGC == 1] <- 1
+
+  dat$attr$rGC.timesInf <- rep(0, num)
+  dat$attr$rGC.timesInf[rGC == 1] <- 1
+  dat$attr$uGC.timesInf <- rep(0, num)
+  dat$attr$uGC.timesInf[uGC == 1] <- 1
+
+  dat$attr$rGC.tx <- dat$attr$uGC.tx <- rep(NA, num)
+  dat$attr$rGC.tx.prep <- dat$attr$uGC.tx.prep <- rep(NA, num)
+
+  # Initialize CT infection at both sites
+  idsUCT <- sample(idsUreth, size = round(init$prev.uct * num), FALSE)
+  uCT[idsUCT] <- 1
+
+  idsRCT <- sample(setdiff(idsRect, idsUCT), size = round(init$prev.rct * num), FALSE)
+  rCT[idsRCT] <- 1
+
+  dat$attr$rCT <- rCT
+  dat$attr$uCT <- uCT
+
+  dat$attr$rCT.sympt <- dat$attr$uCT.sympt <- rep(NA, num)
+  dat$attr$rCT.sympt[rCT == 1] <- rbinom(sum(rCT == 1), 1, dat$param$rct.sympt.prob)
+  dat$attr$uCT.sympt[uCT == 1] <- rbinom(sum(uCT == 1), 1, dat$param$uct.sympt.prob)
+
+  dat$attr$rCT.infTime <- dat$attr$uCT.infTime <- rep(NA, num)
+  dat$attr$rCT.infTime[dat$attr$rCT == 1] <- 1
+  dat$attr$uCT.infTime[dat$attr$uCT == 1] <- 1
+
+  dat$attr$rCT.timesInf <- rep(0, num)
+  dat$attr$rCT.timesInf[rCT == 1] <- 1
+  dat$attr$uCT.timesInf <- rep(0, num)
+  dat$attr$uCT.timesInf[uCT == 1] <- 1
+
+  dat$attr$rCT.tx <- dat$attr$uCT.tx <- rep(NA, num)
+  dat$attr$rCT.tx.prep <- dat$attr$uCT.tx.prep <- rep(NA, num)
 
   return(dat)
 
