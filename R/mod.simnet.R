beb1acb992d01629f0a150302e95cdeefe916481
Samuel Jenness <smjenness@gmail.com>
2018-12-18 18:55:37 -0500

    Revised simnet_msm fx

--- R/mod.simnet.R
+++ R/mod.simnet.R
@@ -20,11 +20,7 @@ simnet_msm <- function(dat, at) {
   ## Main network
   nwparam.m <- EpiModel::get_nwparam(dat, network = 1)
 
-  if (dat$param$method == 1) {
-    dat$attr$deg.pers <- get_degree(dat$el[[2]])
-  } else {
-    dat$attr$deg.pers <- paste0(dat$attr$race, get_degree(dat$el[[2]]))
-  }
+  dat$attr$deg.pers <- get_degree(dat$el[[2]])
   dat <- tergmLite::updateModelTermInputs(dat, network = 1)
 
   dat$el[[1]] <- tergmLite::simulate_network(p = dat$p[[1]],
@@ -39,11 +35,7 @@ simnet_msm <- function(dat, at) {
   ## Casual network
   nwparam.p <- EpiModel::get_nwparam(dat, network = 2)
 
-  if (dat$param$method == 1) {
-    dat$attr$deg.main <- get_degree(dat$el[[1]])
-  } else {
-    dat$attr$deg.main <- paste0(dat$attr$race, get_degree(dat$el[[1]]))
-  }
+  dat$attr$deg.main <- get_degree(dat$el[[1]])
   dat <- tergmLite::updateModelTermInputs(dat, network = 2)
 
   dat$el[[2]] <- tergmLite::simulate_network(p = dat$p[[2]],
@@ -63,11 +55,7 @@ simnet_msm <- function(dat, at) {
   ## One-off network
   nwparam.i <- EpiModel::get_nwparam(dat, network = 3)
 
-  if (dat$param$method == 1) {
-    dat$attr$deg.pers <- get_degree(dat$el[[2]])
-  } else {
-    dat$attr$deg.pers <- paste0(dat$attr$race, get_degree(dat$el[[2]]))
-  }
+  dat$attr$deg.tot <- dat$attr$deg.main + get_degree(dat$el[[2]])
   dat <- tergmLite::updateModelTermInputs(dat, network = 3)
 
   dat$el[[3]] <- tergmLite::simulate_ergm(p = dat$p[[3]],
