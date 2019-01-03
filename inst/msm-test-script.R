21b3533dc44eaca59fb556fec156cad3eaace151
Samuel Jenness <smjenness@gmail.com>
2019-01-03 10:56:35 -0500

    Update test script

--- inst/msm-test-script.R
+++ inst/msm-test-script.R
@@ -14,28 +14,57 @@ file.info(file.path(scr.dir, "data/artnet.NetEst.Atlanta.rda"))$mtime >
   file.info(file.path(scr.dir, "data/artnet.NetStats.Atlanta.rda"))$mtime
 
 param <- param_msm(netstats = netstats,
-                   riskh.start = 500,
-                   prep.start = 500,
-                   prep.start.prob = 0.2,
-                   prep.adhr.dist = c(0.089, 0.127, 0.785),
-                   prep.adhr.hr = c(0.69, 0.19, 0.02),
-                   prep.discont.rate = 1 - (2^(-1/781)),
-                   prep.tst.int = 90,
-                   prep.risk.int = 182,
-                   prep.sti.screen.int = 182,
-                   prep.sti.prob.tx = 1,
-                   a.rate = 0.0004 / 7)
+                   hiv.test.int = c(301, 315),
+                   a.rate = 0.00055 / 7,
+                   riskh.start = 2,
+                   prep.start = 30,
+                   prep.start.prob = 0.10,
+                   tt.part.supp = c(0.20, 0.20),
+                   tt.full.supp = c(0.40, 0.40),
+                   tt.dur.supp = c(0.40, 0.40),
+                   tx.halt.full.rr = 0.8,
+                   tx.halt.dur.rr = 0.1,
+                   tx.reinit.full.rr = 2.0,
+                   tx.reinit.dur.rr = 5.0,
+                   hiv.rgc.rr = 2.5,
+                   hiv.ugc.rr = 1.5,
+                   hiv.rct.rr = 2.5,
+                   hiv.uct.rr = 1.5,
+                   hiv.dual.rr = 0.0)
 init <- init_msm(init.hiv.mod = epistats$hiv.mod)
 control <- control_msm(simno = 1,
-                       nsteps = 520,
+                       nsteps = 52 * 25,
                        nsims = 1,
                        ncores = 1)
 
-# sim <- netsim(est, param, init, control)
+sim <- netsim(est, param, init, control)
 
-# df <- as.data.frame(sim)
-# names(df)
+df <- as.data.frame(sim)
+names(df)
 
+par(mar = c(3,3,1,1), mgp = c(2,1,0))
+plot(sim, y = "i.prev", mean.smooth = FALSE)
+plot(sim, y = "num")
+plot(sim, y = "dep.gen", mean.smooth = FALSE)
+plot(sim, y = "dep.AIDS", mean.smooth = FALSE)
+plot(sim, y = "prepCurr")
+plot(sim, y = "cc.dx", mean.smooth = FALSE)
+plot(sim, y = "cc.linked", mean.smooth = FALSE, ylim = c(0.8, 1))
+plot(sim, y = "cc.linked1m", mean.smooth = FALSE)
+plot(sim, y = "cc.tx", mean.smooth = FALSE)
+plot(sim, y = "cc.tx.any1y", mean.smooth = FALSE)
+plot(sim, y = "cc.vsupp", mean.smooth = FALSE)
+plot(sim, y = "cc.vsupp.tt1", mean.smooth = FALSE)
+plot(sim, y = "cc.vsupp.tt2", mean.smooth = FALSE)
+plot(sim, y = "cc.vsupp.tt3", mean.smooth = FALSE)
+plot(sim, y = "cc.vsupp.dur1y", mean.smooth = FALSE)
+
+plot(sim, y = "hstage.acute", mean.smooth = FALSE)
+plot(sim, y = "hstage.chronic", mean.smooth = FALSE)
+plot(sim, y = "hstage.aids", mean.smooth = FALSE)
+
+plot(sim, y = "ir100.gc", mean.smooth = FALSE, ylim = c(0, 10))
+plot(sim, y = "ir100.ct", mean.smooth = FALSE, ylim = c(0, 10))
 
 # Testing/Timing ------------------------------------------------------
 
@@ -44,7 +73,7 @@ print(m, unit = "ms")
 
 dat <- initialize_msm(est, param, init, control, s = 1)
 
-for (at in 2:520) {
+for (at in 2:200) {
   dat <- aging_msm(dat, at)
   dat <- departure_msm(dat, at)
   dat <- arrival_msm(dat, at)
@@ -69,3 +98,11 @@ nrow(dat$temp$plist)
 table(dat$temp$plist[, "start"])
 table(dat$temp$plist[, "stop"])
 head(dat$temp$plist)
+
+plist <- as.data.frame(dat$temp$plist)
+pmain <- filter(plist, ptype == 2)
+table(pmain$start)
+hist(pmain$start)
+
+
+
