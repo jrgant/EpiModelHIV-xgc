e1bc845512aeac377f04f761e32916eaa7cb0265
Samuel Jenness <smjenness@gmail.com>
2019-05-25 10:49:46 -0400

    Tail 500 for R0.mean.cens

--- R/mod.prevalence.R
+++ R/mod.prevalence.R
@@ -55,7 +55,7 @@ prevalence_msm <- function(dat, at) {
   dat$epi$ir100[at] <- (dat$epi$incid[at] / sum(status == 0, dat$epi$incid[at], na.rm = TRUE)) * 5200
 
   dat$epi$R0.mean.cs[at] <- mean(dat$attr$count.trans[status == 1], na.rm = TRUE)
-  dat$epi$R0.mean.cens[at] <- suppressWarnings(mean(dat$temp$R0, na.rm = TRUE))
+  dat$epi$R0.mean.cens[at] <- suppressWarnings(mean(tail(dat$temp$R0, 500), na.rm = TRUE))
 
   # Care continuum stats
   dat$epi$cc.dx[at] <- sum(dat$attr$diag.status == 1, na.rm = TRUE) /
