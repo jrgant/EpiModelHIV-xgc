44cb2adc2d4ce7861d66f2510f23d6e009004744
jrgant <jason.gantenberg@gmail.com>
2020-05-11 13:01:54 -0400

    initial addition of pharyngeal GC

--- R/mod.stitx.R
+++ R/mod.stitx.R
@@ -24,17 +24,29 @@ stitx_msm <- function(dat, at) {
   race <- dat$attr$race
 
   ## Symptomatic GC Treatment ##
-  idsRGC_tx_sympt <- which(dat$attr$rGC == 1 &
-                           dat$attr$rGC.infTime < at &
-                           dat$attr$rGC.sympt == 1 &
-                           is.na(dat$attr$rGC.tx))
-  idsUGC_tx_sympt <- which(dat$attr$uGC == 1 &
-                           dat$attr$uGC.infTime < at &
-                           dat$attr$uGC.sympt == 1 &
-                           is.na(dat$attr$uGC.tx))
+  idsRGC_tx_sympt <- which(
+    dat$attr$rGC == 1 &
+    dat$attr$rGC.infTime < at &
+    dat$attr$rGC.sympt == 1 &
+    is.na(dat$attr$rGC.tx)
+  )
+
+  idsUGC_tx_sympt <- which(
+    dat$attr$uGC == 1 &
+    dat$attr$uGC.infTime < at &
+    dat$attr$uGC.sympt == 1 &
+    is.na(dat$attr$uGC.tx)
+  )
+
+  idsPGC_tx_sympt <- which(
+    dat$attr$pGC == 1 &
+    dat$attr$pGC.infTime < at &
+    dat$attr$pGC.sympt == 1 &
+    is.na(dat$attr$uGC.tx)
+  )
 
   # Subset by race
-  idsGC_tx_sympt <- union(idsRGC_tx_sympt, idsUGC_tx_sympt)
+  idsGC_tx_sympt <- union(idsRGC_tx_sympt, idsUGC_tx_sympt, idsPGC_tx_sympt)
   races <- sort(unique(race[idsGC_tx_sympt]))
   txGC_sympt <- rep(NA, length(idsGC_tx_sympt))
   for (i in races) {
@@ -43,24 +55,38 @@ stitx_msm <- function(dat, at) {
   }
   ids_txGC_sympt <- idsGC_tx_sympt[which(txGC_sympt == 1)]
 
-  # Subset by site
+  # Subset by anatomic site
   txRGC_sympt <- intersect(idsRGC_tx_sympt, ids_txGC_sympt)
   txUGC_sympt <- intersect(idsUGC_tx_sympt, ids_txGC_sympt)
+  txPGC_sympt <- intersect(idsPGC_tx_sympt, ids_txGC_sympt)
 
   ## Asymptomatic GC Treatment ##
-  idsRGC_tx_asympt <- which(dat$attr$rGC == 1 &
-                            dat$attr$rGC.infTime < at &
-                            dat$attr$rGC.sympt == 0 &
-                            is.na(dat$attr$rGC.tx) &
-                            dat$attr$prepStat == 0)
-  idsUGC_tx_asympt <- which(dat$attr$uGC == 1 &
-                            dat$attr$uGC.infTime < at &
-                            dat$attr$uGC.sympt == 0 &
-                            is.na(dat$attr$uGC.tx) &
-                            dat$attr$prepStat == 0)
+  idsRGC_tx_asympt <- which(
+    dat$attr$rGC == 1 &
+    dat$attr$rGC.infTime < at &
+    dat$attr$rGC.sympt == 0 &
+    is.na(dat$attr$rGC.tx) &
+    dat$attr$prepStat == 0
+  )
+
+  idsUGC_tx_asympt <- which(
+    dat$attr$uGC == 1 &
+    dat$attr$uGC.infTime < at &
+    dat$attr$uGC.sympt == 0 &
+    is.na(dat$attr$uGC.tx) &
+    dat$attr$prepStat == 0
+  )
+
+  idsPGC_tx_asympt <- which(
+    dat$attr$pGC == 1 &
+    dat$attr$pGC.infTime < at &
+    dat$attr$pGC.sympt == 0 &
+    is.na(dat$attr$pGC.tx) &
+    dat$attr$prepStat == 0
+  )
 
   # Subset by race
-  idsGC_tx_asympt <- union(idsRGC_tx_asympt, idsUGC_tx_asympt)
+  idsGC_tx_asympt <- union(idsRGC_tx_asympt, idsUGC_tx_asympt, idsPGC_tx_asympt)
   races <- sort(unique(race[idsGC_tx_asympt]))
   txGC_asympt <- rep(NA, length(idsGC_tx_asympt))
   for (i in races) {
@@ -72,17 +98,19 @@ stitx_msm <- function(dat, at) {
   # Subset by site
   txRGC_asympt <- intersect(idsRGC_tx_asympt, ids_txGC_asympt)
   txUGC_asympt <- intersect(idsUGC_tx_asympt, ids_txGC_asympt)
+  txPGC_asympt <- intersect(idsPGC_tx_asympt, ids_txGC_asympt)
 
   ## All Treated GC ##
 
   # IDs of men sucessfully treated
   txRGC <- union(txRGC_sympt, txRGC_asympt)
   txUGC <- union(txUGC_sympt, txUGC_asympt)
+  txPGC <- union(txPGC_sympt, txPGC_asympt)
 
   # IDs of men eligible for treatment
   idsRGC_tx <- union(idsRGC_tx_sympt, idsRGC_tx_asympt)
   idsUGC_tx <- union(idsUGC_tx_sympt, idsUGC_tx_asympt)
-
+  idsPGC_tx <- union(idsPGC_tx_sympt, idsPGC_tx_asympt)
 
   ## Interval-based treatment for MSM on PrEP ##
   idsSTI_screen <- which(dat$attr$prepStartTime == at |
