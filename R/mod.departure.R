cc1a7edd0420198fc51c1ed3883048d13a3c7d38
jrgant <jason.gantenberg@gmail.com>
2020-09-04 03:42:47 +0100

    drop hetero deaths module

--- R/mod.departure.R
+++ R/mod.departure.R
@@ -43,7 +43,7 @@ departure_msm <- function(dat, at) {
     ages <- age[ids.race]
     rates[ids.race] <- asmr[match(ages, asmr$age), i + 1, with = FALSE][[1]]
   }
-
+ 
   idsDep <- idsElig[rbinom(length(rates), 1, rates) == 1]
 
   ## HIV-related deaths
@@ -60,6 +60,7 @@ departure_msm <- function(dat, at) {
       dat$el[[i]] <- tergmLite::delete_vertices(dat$el[[i]], idsDepAll)
     }
     dat$attr <- deleteAttr(dat$attr, idsDepAll)
+
     if (unique(sapply(dat$attr, length)) != attributes(dat$el[[1]])$n) {
       stop("mismatch between el and attr length in departures mod")
     }
@@ -75,104 +76,12 @@ departure_msm <- function(dat, at) {
   }
 
   ## Summary Output
+  ## FIXME should this be using idsDep where not in idsDepAIDS? Check and update
+  ##       techdocs if needed.
   dat$epi$dep.gen[at] <- length(idsDep)
   dat$epi$dep.AIDS[at] <- length(idsDepAIDS)
   dat$epi$dep.HIV[at] <- length(depHIV)
   dat$epi$dep.HIV.old[at] <- length(depHIV.old)
-
-  return(dat)
-}
-
-
-#' @export
-#' @rdname departure_msm
-deaths_het <- function(dat, at) {
-
-  ### 1. Susceptible Deaths ###
-
-  ## Variables
-  male <- dat$attr$male
-  age <- dat$attr$age
-  cd4Count <- dat$attr$cd4Count
-
-  di.cd4.aids <- dat$param$di.cd4.aids
-  ds.exit.age <- dat$param$ds.exit.age
-
-  ## Eligible are: active uninf, pre-death infected, unhealthy old
-  idsEligSus <- which((is.na(cd4Count) |
-                       cd4Count > di.cd4.aids |
-                       (cd4Count <= di.cd4.aids & age > ds.exit.age)))
-  nEligSus <- length(idsEligSus)
-
-  # Set age-sex specific rates
-  ds.rates <- dat$param$ds.rates
-  if (nEligSus > 0) {
-    rates <- ds.rates$mrate[100*male[idsEligSus] + age[idsEligSus]]
-  }
-
-
-  ## Process
-  nDeathsSus <- 0; idsDeathsSus <- NULL
-  if (nEligSus > 0) {
-    vecDeathsSus <- which(rbinom(nEligSus, 1, rates) == 1)
-    nDeathsSus <- length(vecDeathsSus)
-  }
-
-
-  ## Update Attributes
-  if (nDeathsSus > 0) {
-    idsDeathsSus <- idsEligSus[vecDeathsSus]
-    dat$attr$active[idsDeathsSus] <- 0
-  }
-
-
-  ### 2. Infected Deaths ###
-
-  ## Variables
-  active <- dat$attr$active
-  di.cd4.rate <- dat$param$di.cd4.rate
-
-  ## Process
-  nDeathsInf <- 0; idsDeathsInf <- NULL
-
-  cd4Count <- dat$attr$cd4Count
-  stopifnot(length(active) == length(cd4Count))
-
-  idsEligInf <- which(active == 1 & cd4Count <= di.cd4.aids)
-  nEligInf <- length(idsEligInf)
-
-  if (nEligInf > 0) {
-    vecDeathsInf <- which(rbinom(nEligInf, 1, di.cd4.rate) == 1)
-    if (length(vecDeathsInf) > 0) {
-      idsDeathsInf <- idsEligInf[vecDeathsInf]
-      nDeathsInf <- length(idsDeathsInf)
-    }
-  }
-
-  idsDeathsDet <- which(cd4Count <= 0)
-  if (length(idsDeathsDet) > 0) {
-    idsDeathsInf <- c(idsDeathsInf, idsDeathsDet)
-    nDeathsInf <- nDeathsInf + length(idsDeathsDet)
-  }
-
-
-  ### 3. Update Attributes ###
-  if (nDeathsInf > 0) {
-    dat$attr$active[idsDeathsInf] <- 0
-  }
-
-  ## 4. Update Population Structure ##
-  inactive <- which(dat$attr$active == 0)
-  dat$el[[1]] <- tergmLite::delete_vertices(dat$el[[1]], inactive)
-  dat$attr <- deleteAttr(dat$attr, inactive)
-
-  if (unique(sapply(dat$attr, length)) != attributes(dat$el[[1]])$n) {
-    stop("mismatch between el and attr length in death mod")
-  }
-
-  ### 5. Summary Statistics ###
-  dat$epi$ds.flow[at] <- nDeathsSus
-  dat$epi$di.flow[at] <- nDeathsInf
 
   return(dat)
 }
