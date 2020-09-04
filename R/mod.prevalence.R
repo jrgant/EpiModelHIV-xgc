d4aec4d2a32a236d246b7a1f63c1e9fb8852570e
jrgant <jason.gantenberg@gmail.com>
2020-09-04 03:56:02 +0100

    drop het prevalence tracking module

--- R/mod.prevalence.R
+++ R/mod.prevalence.R
@@ -403,58 +403,6 @@ prevalence_msm <- function(dat, at) {
   return(dat)
 }
 
-#' @export
-#' @rdname prevalence_msm
-prevalence_het <- function(dat, at) {
-
-  status <- dat$attr$status
-  male <- dat$attr$male
-  age <- dat$attr$age
-
-  nsteps <- dat$control$nsteps
-  rNA <- rep(NA, nsteps)
-
-  # Initialize vectors
-  if (at == 1) {
-    dat$epi$i.num <- rNA
-    dat$epi$num <- rNA
-
-    dat$epi$i.num.male <- rNA
-    dat$epi$i.num.feml <- rNA
-    dat$epi$i.prev.male <- rNA
-    dat$epi$i.prev.feml <- rNA
-
-    dat$epi$num.male <- rNA
-    dat$epi$num.feml <- rNA
-    dat$epi$meanAge <- rNA
-    dat$epi$propMale <- rNA
-
-    dat$epi$si.flow <- rNA
-    dat$epi$si.flow.male <- rNA
-    dat$epi$si.flow.feml <- rNA
-
-    dat$epi$b.flow <- rNA
-    dat$epi$ds.flow <- dat$epi$di.flow <- rNA
-  }
-
-  dat$epi$i.num[at] <- sum(status == 1, na.rm = TRUE)
-  dat$epi$num[at] <- length(status)
-
-  dat$epi$i.num.male[at] <- sum(status == 1 & male == 1, na.rm = TRUE)
-  dat$epi$i.num.feml[at] <- sum(status == 1 & male == 0, na.rm = TRUE)
-  dat$epi$i.prev.male[at] <- sum(status == 1 & male == 1, na.rm = TRUE) /
-    sum(male == 1, na.rm = TRUE)
-  dat$epi$i.prev.feml[at] <- sum(status == 1 & male == 0, na.rm = TRUE) /
-    sum(male == 0, na.rm = TRUE)
-
-  dat$epi$num.male[at] <- sum(male == 1, na.rm = TRUE)
-  dat$epi$num.feml[at] <- sum(male == 0, na.rm = TRUE)
-  dat$epi$meanAge[at] <- mean(age, na.rm = TRUE)
-  dat$epi$propMale[at] <- mean(male, na.rm = TRUE)
-
-  return(dat)
-}
-
 
 whichVlSupp <- function(attr, param) {
   which(attr$status == 1 &
